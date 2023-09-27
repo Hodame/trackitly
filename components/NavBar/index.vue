c6836294c38900434638826bc081ca38b660377f
hodame <hodame04@gmail.com>
2023-09-27 17:48:36 +0500

    create navbar

---
+++ components/NavBar/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full bg-gray-100 py-5 pl-10">
+    <div class="max-w-xs">
+      <BaseInput placeholder="Search for tasks" left-icon="ph:magnifying-glass" />
+    </div>
+  </div>
+</template>
