d723cadce22a8df75755c845334ba480568c48eb
hodame <hodame04@gmail.com>
2023-09-27 17:49:32 +0500

    add navbar and animation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-const sidebarOpened = ref(true);
+const isSidebar = useSidebar();
 </script>
 
 <template>
   <div
-    class="grid h-screen"
-    :class="sidebarOpened ? 'grid-cols-[270px,1fr]' : 'grid-cols-[70px,1fr]'"
+    class="grid h-screen transition-all duration-300"
+    :class="isSidebar ? 'grid-cols-[270px,1fr]' : 'grid-cols-[100px,1fr]'"
   >
     <Sidebar />
     <div>
-      <slot/>
+      <NavBar />
+      <slot />
     </div>
   </div>
 </template>
