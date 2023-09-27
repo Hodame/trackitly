337365e4442216f277bd621eb918753bfd1618ad
hodame <hodame04@gmail.com>
2023-09-27 17:48:50 +0500

    create user tile

---
+++ components/Sidebar/User.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+type Props = {
+  name: string;
+  avatar: string | undefined;
+};
+
+defineProps<Props>();
+
+const isSidebar = useSidebar();
+</script>
+
+<template>
+  <div class="flex items-center gap-3" :class="{ 'justify-center': !isSidebar }">
+    <div
+      class="flex h-14 w-14 items-center justify-center rounded-full bg-gray-200"
+      :title="!isSidebar ? name : ''"
+    >
+      <img v-if="avatar" :src="avatar" :alt="name" />
+      <Icon v-else name="ph:user" size="1.5rem" />
+    </div>
+
+    <span v-if="isSidebar" class="font-medium animate-after-sidebar-delay">{{ name }}</span>
+  </div>
+</template>
