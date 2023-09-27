646292c468ea4ccdfc4139f785aa0f3dd04cd8ce
hodame <hodame04@gmail.com>
2023-09-27 17:49:53 +0500

    setup configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-icon'],
+  modules: ['nuxt-icon', '@vueuse/nuxt'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {},
-    },
+      autoprefixer: {}
+    }
   },
-})
+  vite: {
+    vue: {
+        script: {
+            defineModel: true,
+            propsDestructure: true
+        }
+    }
+}
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue'
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'after-sidebar-delay': 'after-sidebar-delay 0.6s linear'
+      },
+      keyframes: {
+        'after-sidebar-delay': {
+          '0%': { opacity: 0, width: '0px', position: 'absolute' },
+          '50%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        }
+      }
+    }
   },
-  plugins: [],
-}
+};
