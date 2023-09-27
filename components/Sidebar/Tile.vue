41932608b4f76ebe44cf5e0ca0ccb6c928b293e7
hodame <hodame04@gmail.com>
2023-09-27 17:49:12 +0500

    modify sidebar

--- components/Sidebar/Tile.vue
+++ components/Sidebar/Tile.vue
@@ -1,27 +1,76 @@
 <script setup lang="ts">
+import { StyleValue } from 'nuxt/dist/app/compat/capi';
+
 type Props = {
   text: string;
   to: string;
-  opened?: boolean;
   icon?: string;
 };
 
+type Slots = {
+  nestedLinks?(): void;
+};
+
 defineProps<Props>();
-defineSlots()
+const slots = defineSlots<Slots>();
+
+const isSidebar = useSidebar();
+const isNestedOpened = ref(false);
+const nestedLinks = ref<HTMLDivElement>();
+const tile = ref<HTMLDivElement>();
+
+const { height } = useElementSize(nestedLinks);
+const { height: tileHeight } = useElementSize(tile);
+
+const nestedLinksStyle = computed<StyleValue>(() => {
+  if(!isSidebar.value) isNestedOpened.value = false
+  
+  if (!isNestedOpened.value ) {
+    return {
+      height: `${tileHeight.value}px`
+    };
+  }
+
+  return {
+    height: `${tileHeight.value + height.value}px`
+  };
+});
 </script>
 
 <template>
-  <NuxtLink
-    :to="to"
-    class="group flex w-full items-center gap-1.5 rounded-2xl px-4 py-3 text-left hover:bg-blue-100 [&.router-link-active]:bg-blue-700"
-  >
-    <span v-if="icon" class="text-black group-[.router-link-active]:text-white">
-      <Icon :name="icon" size="1.5rem" />
-    </span>
-    <span class="font-medium text-black group-[.router-link-active]:text-white">{{ text }}</span>
-  </NuxtLink>
+  <div :style="nestedLinksStyle" class="box-content overflow-hidden transition-all">
+    <div ref="tile" class="flex items-center gap-2">
+      <NuxtLink
+        class="group my-1 flex w-full items-center gap-1.5 rounded-2xl px-4 py-3 text-left hover:bg-blue-100 [&.router-link-active]:bg-blue-700"
+        :to="to"
+        :class="{ 'justify-center': !isSidebar }"
+      >
+        <span v-if="icon" class="text-black group-[.router-link-active]:text-white">
+          <Icon :name="icon" size="1.5rem" />
+        </span>
+        <span
+          v-if="isSidebar"
+          class="font-medium text-black group-[.router-link-active]:text-white"
+        >
+          {{ text }}
+        </span>
+      </NuxtLink>
 
-  <div>
-    <slot name="nestedLinks" />
+      <button
+        v-if="isSidebar && slots.nestedLinks"
+        @click="isNestedOpened = !isNestedOpened"
+        class="rounded-full bg-gray-200 px-2 py-1"
+      >
+        <Icon
+          class="-translate-y-[0.1rem] transition-all"
+          :class="{ 'rotate-180': isNestedOpened }"
+          name="ph:caret-down"
+        />
+      </button>
+    </div>
+
+    <div v-if="isSidebar" ref="nestedLinks" class="ml-5">
+      <slot name="nestedLinks" />
+    </div>
   </div>
 </template>
--- components/Sidebar/index.vue
+++ components/Sidebar/index.vue
@@ -1,9 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const isSidebar = useSidebar();
+const selected = useProject();
+const options = ref([
+  { id: 1, title: 'EX' },
+  { id: 2, title: 'MoM' }
+]);
+</script>
 
 <template>
-  <div class="p-5 bg-gray-100 shadow-xl flex flex-col gap-2.5">
-    <SidebarTile to="/" text="Dashboard" icon="ph:house" />
-    <SidebarTile to="tasks" text="Tasks" icon="ph:list-bullets" />
-    <SidebarTile to="users" text="Users" icon="ph:users" />
+  <div class="relative h-screen bg-gray-100 p-5 shadow-xl">
+    <SidebarUser avatar="" name="Sergey Bondar" />
+    <BaseSelect v-if="isSidebar" v-model="selected" label="Project" :options="options" />
+
+    <div class="mt-2 flex-auto">
+      <SidebarTile to="/" text="Dashboard" icon="ph:house" />
+      <SidebarTile to="tasks" text="Tasks" icon="ph:list-bullets">
+        <template #nestedLinks>
+          <SidebarTile to="/" text="Dashboard" icon="ph:house">
+            <template #nestedLinks>
+              <SidebarTile to="users" text="Users" icon="ph:users" />
+            </template>
+          </SidebarTile>
+        </template>
+      </SidebarTile>
+      <SidebarTile to="users" text="Users" icon="ph:users" />
+    </div>
+
+    <button
+      @click="isSidebar = !isSidebar"
+      class="absolute right-0 top-5 flex h-8 w-8 translate-x-1/2 items-center justify-center rounded-full bg-blue-600 text-white"
+    >
+      <Icon
+        name="ph:arrow-right-bold"
+        size="1rem"
+        class="transition-all"
+        :class="{ 'rotate-180': isSidebar }"
+      />
+    </button>
   </div>
 </template>
